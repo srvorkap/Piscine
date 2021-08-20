9cd3f6995029acc5b53303de129b3bd8ba76868a
Srdan <srdan.vorkapic@outlook.com>
2021-08-20 00:46:59 -0700

    ex03

---
+++ day13/ex03/btree_apply_suffix.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   btree_apply_suffix.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: srvorkap <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/07/26 03:31:11 by srvorkap          #+#    #+#             */
+/*   Updated: 2019/07/26 03:36:01 by srvorkap         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_btree.h"
+
+void	btree_apply_suffix(t_btree *root, void (*applyf)(void *))
+{
+		if (root->left)
+			btree_apply_suffix(root->left, applyf);
+		if (root->right)
+			btree_apply_suffix(root->right, applyf);
+		applyf(root->item);
+}
